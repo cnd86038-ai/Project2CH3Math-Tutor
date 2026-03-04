8b971b73b224cab20c7771169f5982bd0533ffe6
cnd86038-ai <cnd86038@email.vccs.edu>
2026-03-04 12:08:21 -0500

    Included random number value from generator, num1 and num2.

--- Project2CH3Math Tutor/main.cpp
+++ Project2CH3Math Tutor/main.cpp
@@ -8,5 +8,8 @@ int main()
 	random_device rd;  // Seed for random number generator
 	mt19937 gen(rd()); // Mersenne Twister engine
 	uniform_int_distribution<> dis(1, 100); // Distribution for numbers between 1 and 100
+
+	int num1 = dis(gen); // Generate first random number
+	int num2 = dis(gen); // Generate second random number
 	return 0;
 }
