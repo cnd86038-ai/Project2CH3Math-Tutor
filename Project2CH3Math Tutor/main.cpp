bd83178ace8f8f4f4f4d536b264ca798d2bfdb32
cnd86038-ai <cnd86038@email.vccs.edu>
2026-03-04 12:07:02 -0500

    Included a range for the numbers to be set between.

--- Project2CH3Math Tutor/main.cpp
+++ Project2CH3Math Tutor/main.cpp
@@ -7,5 +7,6 @@ int main()
 {
 	random_device rd;  // Seed for random number generator
 	mt19937 gen(rd()); // Mersenne Twister engine
+	uniform_int_distribution<> dis(1, 100); // Distribution for numbers between 1 and 100
 	return 0;
 }
